b53000ebcab284beca9da6eade7b84285aa9f33b
dan <dan@lighttwist-software.com>
2025-12-05 22:31:50 +1100

    please added

--- Echidna/Echidna.cpp
+++ Echidna/Echidna.cpp
@@ -8,6 +8,7 @@ int main()
     std::cout << "Hello World of Beautiful Echidnas!\n";
     std::cout << "Now.\n"; 
     std::cout << "Dance!\n"; 
+    std::cout << "Please?\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
